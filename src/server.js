fa48a010f997bc1dfd907a3afb0b1b986db98877
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-08 01:55:31 +0900

    🔨 Configure morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 4000;
+
+const logger = morgan('dev');
 
 const handleHome = (req, res) => {
   res.send('Hello Express');
 };
 
+app.use(logger);
 app.get('/', handleHome);
 
 const handleListening = () => {
