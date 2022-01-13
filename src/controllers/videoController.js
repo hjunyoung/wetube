830a8d28fec65692861a1265de23283787eca716
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-13 23:08:44 +0900

    ⚰️ Remove a fake video object

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,19 @@
-let videos = [
-  {
-    title: 'First Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
-  },
-  {
-    title: 'Second Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 2,
-  },
-  {
-    title: 'Third Video',
-    rating: 5,
-    comments: 2,
-    createdAt: '2 minutes ago',
-    views: 59,
-    id: 3,
-  },
-];
-
 export const trending = (req, res) => {
-  return res.render('home', { pageTitle: 'Home', videos });
+  return res.render('home', { pageTitle: 'Home' });
 };
 
 export const watch = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  return res.render('watch', { pageTitle: `Watch ${video.title}`, video });
+  return res.render('watch', { pageTitle: `Watch` });
 };
 
 export const getEdit = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  return res.render('edit', { pageTitle: `Edit ${video.title}`, video });
+  return res.render('edit', { pageTitle: `Edit` });
 };
 
 export const postEdit = (req, res) => {
@@ -52,6 +23,5 @@ export const postEdit = (req, res) => {
   const {
     body: { title: newTitle },
   } = req;
-  videos[id - 1].title = newTitle;
   return res.redirect(`/videos/${id}`);
 };
