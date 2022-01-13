d65c59bb25836d23a95da31fe507816a80e82041
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-13 22:55:35 +0900

    ♻️ Move init code from server.js to init.js
    Moved codes that have nothing to do with server configuration to init.js.
    Changed a dev script in package.json so that package.json can find init.js.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import './db';
-import './models/Video';
 import express from 'express';
 import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
@@ -7,7 +5,6 @@ import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
 const app = express();
-const PORT = 4000;
 const logger = morgan('dev');
 
 app.set('view engine', 'pug');
@@ -19,8 +16,4 @@ app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-const handleListening = () => {
-  console.log(`✅ Server listening on port http://localhost:${PORT}`);
-};
-
-app.listen(PORT, handleListening);
+export default app;
