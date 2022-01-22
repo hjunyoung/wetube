7e921ad6a065bc618b63226b6771fd82d48a0860
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-22 19:41:05 +0900

    🥅 Catch join form errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,39 @@ export const getJoin = (req, res) => {
 
 export const postJoin = async (req, res) => {
   const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = 'Join';
+
+  if (password !== password2) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'Password confirmation does not match.',
+    });
+  }
+
+  const exists = await User.exists({ username, email });
+  if (exists) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'Both Username and Email already taken.',
+    });
+  }
+
+  const usernameExists = await User.exists({ username });
+  if (usernameExists) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'Username already taken.',
+    });
+  }
+
+  const emailExists = await User.exists({ email });
+  if (emailExists) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'Email already taken.',
+    });
+  }
+
   await User.create({ name, email, username, password, password2, location });
   return res.redirect('/login');
 };
