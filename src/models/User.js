7ff025e5be44a7fc888993f0ec4dc0199b4c44a6
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-22 19:40:32 +0900

    🔨 Make name input optional

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, require: true, unique: true },
   username: { type: String, require: true, unique: true },
   password: { type: String, require: true },
-  name: { type: String, required: true },
+  name: { type: String },
   location: String,
 });
 
