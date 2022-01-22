651f6fc2f2d99299177aedf4847a511890253ec0
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-22 19:09:39 +0900

    🔨 Hash user password using bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, require: true, unique: true },
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, require: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre('save', async function () {
+  console.log(this.password);
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log(this.password);
 });
 
 const User = mongoose.model('User', userSchema);
